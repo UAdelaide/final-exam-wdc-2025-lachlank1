dcaf63cc596a0120c0bf12f5af13dd5430c1f26f
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 03:16:48 +0000

    [automatic commit] 2025-06-20 03:16:48 UTC

--- part1/app.js
+++ part1/app.js
@@ -29,7 +29,7 @@ let db;
     });
 
     // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS ');
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
     await connection.end();
 
     // Now connect to the created database
@@ -37,7 +37,7 @@ let db;
       host: 'localhost',
       user: 'root',
       password: '',
-      database: 'testdb'
+      database: 'DogWalkService'
     });
 
     // Create a table if it doesn't exist
