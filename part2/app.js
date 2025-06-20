90a456079b5fe2112a5ec84c5eec3032b565da84
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 11:11:11 +0000

    [automatic commit] 2025-06-20 11:11:11 UTC

--- part2/app.js
+++ part2/app.js
@@ -58,6 +58,6 @@ app.get('/logout', (req, res) => {
 // route for getting dogs
 
 app.get('/dogs', async (req, res) => {
-    if  ()
+    const [rows] 
 // Export the app instead of listening here
 module.exports = app;
