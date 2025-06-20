f19473490c4197853fe5b0cda1ae37060cf2937e
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 03:34:47 +0000

    [automatic commit] 2025-06-20 03:34:47 UTC

--- part1/app.js
+++ part1/app.js
@@ -73,7 +73,7 @@ let db;
         );
     `);
     await db.execute(`
-        CREATE TALB IF NOT EXISTS WalkApplications (
+        CREATE TALBLE IF NOT EXISTS WalkApplications (
             application_id INT AUTO_INCREMENT PRIMARY KEY,
             request_id INT NOT NULL,
             walker_id INT NOT NULL,
@@ -85,7 +85,18 @@ let db;
         );
     `);
     await db.execute(`
-        CREA
+        CREATE TABLE IF NOT EXISTS WalkApplications (
+            rating_id INT AUTO_INCREMENT PRIMARY KEY,
+    request_id INT NOT NULL,
+    walker_id INT NOT NULL,
+    owner_id INT NOT NULL,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    comments TEXT,
+    rated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
+    FOREIGN KEY (walker_id) REFERENCES Users(user_id),
+    FOREIGN KEY (owner_id) REFERENCES Users(user_id),
+    CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
 
     // Insert data if table is empty
     const [userRows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
