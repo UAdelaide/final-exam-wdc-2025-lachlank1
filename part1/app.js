16b0271bf9540fd2a00cff1e2d5335f9fb932cdb
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 03:49:59 +0000

    [automatic commit] 2025-06-20 03:49:59 UTC

--- part1/app.js
+++ part1/app.js
@@ -115,16 +115,6 @@ let db;
       `);
     }
 
-    // Insert data if table is empty
-    // const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
-    // if (rows[0].count === 0) {
-    //   await db.execute(`
-    //     INSERT INTO books (title, author) VALUES
-    //     ('1984', 'George Orwell'),
-    //     ('To Kill a Mockingbird', 'Harper Lee'),
-    //     ('Brave New World', 'Aldous Huxley')
-    //   `);
-    // }
     const [dogRows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
     if (dogRows[0].count === 0){
         await db.exectute(`
