c1db052ba948018591fceadf1119fa61cf70bab1
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 09:22:50 +0000

    [automatic commit] 2025-06-20 09:22:50 UTC

--- part2/app.js
+++ part2/app.js
@@ -8,7 +8,9 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(session())
+app.use(session({
+    sec
+}))
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
