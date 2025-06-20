dbaad74040a84f9c868c514034e622e2b0a78bf6
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 03:38:12 +0000

    [automatic commit] 2025-06-20 03:38:12 UTC ;; communications error to 208.67.222.222#53: timed out

--- part1/app.js
+++ part1/app.js
@@ -129,7 +129,8 @@ let db;
             INSERT INTO Dogs (owner_id, name, size) VALUES
             ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
             ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
-            
+            ((SELECT user_id FROM Users WHERE username = 'bobwalker'), 'Sam', 'large'),
+            ((SELECT user_id FROM Users WHERE username = 'lachlankeep'), 'pibble', 'small'),
         `);
     }
   } catch (err) {
