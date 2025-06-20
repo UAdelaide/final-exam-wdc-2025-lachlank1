642a54791d0aeeae406b3ee6726ddb0404eed8bc
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 05:11:56 +0000

    [automatic commit] 2025-06-20 05:11:55 UTC

--- part1/db.js
+++ part1/db.js
@@ -4,7 +4,7 @@ const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'DogWalkService',
+    database: 'DogWalkService'
 });
 
 module.exports = db;
