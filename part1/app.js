a4550f0162084137d620497e13082fa158ee6a69
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 03:43:46 +0000

    [automatic commit] 2025-06-20 03:43:46 UTC

--- part1/app.js
+++ part1/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,6 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 let db;
 
