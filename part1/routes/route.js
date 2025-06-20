f4f835821f90575b67940b275cf62f7e3dda8a42
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 04:16:39 +0000

    [automatic commit] 2025-06-20 04:16:38 UTC

--- part1/routes/route.js
+++ part1/routes/route.js
@@ -17,8 +17,6 @@ router.get('/walkrequests/open', async (req, res) => {
     const [requests] = await db.query(`
         SELECT wr.request_id as request_id,
         wr.dog_id as dog_name
-        wr.request_time as request_time,
-        wr.duration_minutes as duration_minutes,
         wr.location as location,
         FROM WalkRequests wr
         JOIN Dogs d on wr.dog_id = d.dog_id
