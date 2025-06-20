1c60427ea71830bc8e2ad3067d0167d3ccf77e0f
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 03:25:06 +0000

    [automatic commit] 2025-06-20 03:25:06 UTC

--- part1/app.js
+++ part1/app.js
@@ -56,7 +56,7 @@ let db;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
+INSERT INTO Users (username, email, password_hash, role)
         ('1984', 'George Orwell'),
         ('To Kill a Mockingbird', 'Harper Lee'),
         ('Brave New World', 'Aldous Huxley')
