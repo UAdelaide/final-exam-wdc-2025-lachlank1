f902fb8a2a72a31cd7013ab3258f3d689faebe11
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 11:11:58 +0000

    [automatic commit] 2025-06-20 11:11:58 UTC ;; communications error to 208.67.222.222#53: timed out

--- part2/app.js
+++ part2/app.js
@@ -58,6 +58,6 @@ app.get('/logout', (req, res) => {
 // route for getting dogs
 
 app.get('/dogs', async (req, res) => {
-    const [rows] = await db.query(`SELECT  )
+    const [rows] = await db.query(`SELECT dog_id, name FROM Dogs WHERE owner_id = ?`, [req.session.userid]);
 // Export the app instead of listening here
 module.exports = app;
