a78b29914904145b66e5bc4d2e3f4da4a0f98d99
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 03:18:11 +0000

    [automatic commit] 2025-06-20 03:18:11 UTC

--- part1/app.js
+++ part1/app.js
@@ -41,7 +41,7 @@ let db;
     });
 
     // Create a table if it doesn't exist
-    await db.execute(`
+      await db.execute(`
         CREATE TABLE IF NOT EXISTS Users (
             user_id INT AUTO_INCREMENT PRIMARY KEY,
             username VARCHAR(50) UNIQUE NOT NULL,
