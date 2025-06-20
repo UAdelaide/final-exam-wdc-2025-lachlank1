dd4d68ebe5dba5feece6ac61086294089ac4839b
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 08:38:27 +0000

    [automatic commit] 2025-06-20 08:38:27 UTC

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const db = require('./models/db')
+const db = require('./models/db');
 require('dotenv').config();
 
 const app = express();
