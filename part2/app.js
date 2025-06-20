cf0a0e8e9bd68dcb492e7654c6dabe55744d3920
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 09:52:28 +0000

    [automatic commit] 2025-06-20 09:52:28 UTC

--- part2/app.js
+++ part2/app.js
@@ -30,6 +30,7 @@ app.post('/login', async (req, res) => {
         const [rows] = await db.query(`SELECT user_id, username, role FROM Users WHERE username = ? AND password_hash = ?`, [username, password]);
 
         console.log('Result:', rows);
+        
         if (rows.length === 0 ){
             return res.status(401).json({ error: "Wrong login" });
         }
