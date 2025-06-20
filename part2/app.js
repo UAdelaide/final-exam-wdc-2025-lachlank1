f09261e47503b88d41d438411b52aec3f14aa706
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 08:51:35 +0000

    [automatic commit] 2025-06-20 08:51:35 UTC

--- part2/app.js
+++ part2/app.js
@@ -24,6 +24,7 @@ app.post('/login', (req, res) => {
         if (results.length === 0) {
             return res.status(401).json({ message: 'Invalid username or password' });
         }
+        if (err) return res.status(500).send("")
         // If user is found, return success message
         res.json({ message: 'Login successful', username, password });
         if (results[0].role === 'walker') {
