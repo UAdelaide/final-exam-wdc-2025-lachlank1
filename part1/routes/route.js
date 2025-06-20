fa8d73d4b4530d92d6e727bf80ae1097aa6091dd
lachlank1 <lachlan.keep@student.adelaide.edu.au>
2025-06-20 04:01:58 +0000

    [automatic commit] 2025-06-20 04:01:58 UTC ;; communications error to 208.67.222.222#53: timed out

--- part1/routes/route.js
+++ part1/routes/route.js
@@ -5,7 +5,9 @@ var db = require('../db'); // Import the database connection
 router.get('/dogs', async (req, res) => {
     const [dogs] = await db.query(`
         SELECT d.name AS dog_name,
-            
+        d.size AS size,
+        u.username as owner_username
+        FROM Dogs d
         SELECT d.size AS size,
         FROM Dogs d
         JOIN Users u ON d.owner_id = u.user_name AS owner_name
